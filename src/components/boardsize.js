5970e045064d1ec39008274588fcc6a8880edccf
Justin NORR <jnorthageorr@gmail.com>
2017-08-23 13:36:51 -0400

    adds dropdown menu for boardsize, doesnt work yet. Separates components into their own files, for clarity

---
+++ src/components/boardsize.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+class Boardsize extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { value: '10x10'};
+
+		this.handleChange = this.handleChange.bind(this); 
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+	componentDidMount(){
+
+	}
+	handleChange(event) {
+		this.setState({value: event.target.value});
+	}
+	handleSubmit(event) {
+		event.preventDefault();
+	}
+	render() {
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<label>
+					Pick board size: 
+					<select value={this.state.value} onChange={this.handleChange}>
+						<option value="10x10">10x10</option>
+						<option value="20x20">20x20</option>
+					</select>
+				</label>
+				<input type="submit" value="set" />
+			</form>
+
+		)
+	}
+}
+
+Boardsize.propTypes = {};
+
+export default Boardsize;
