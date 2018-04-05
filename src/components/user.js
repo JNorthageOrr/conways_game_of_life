27990595da0fc3d53109bcc19e6961bd40d8edec
Justin NORR <jnorthageorr@gmail.com>
2018-04-05 11:28:26 -0400

    adds error message parsing for login ajax request

--- src/components/user.js
+++ src/components/user.js
@@ -39,7 +39,25 @@ class App extends Component {
         success: function (result) {
           console.log(result)
           localStorage.setItem("jwt", result.jwt)
-          //browserHistory.push("/");
+        },
+        error: function(jqXHR, exception) {
+          var msg = '';
+          if (jqXHR.status === 0) {
+              msg = 'Not connect.\n Verify Network.';
+          } else if (jqXHR.status == 404) {
+              msg = 'Requested page not found. [404]';
+          } else if (jqXHR.status == 500) {
+              msg = 'Internal Server Error [500].';
+          } else if (exception === 'parsererror') {
+              msg = 'Requested JSON parse failed.';
+          } else if (exception === 'timeout') {
+              msg = 'Time out error.';
+          } else if (exception === 'abort') {
+              msg = 'Ajax request aborted.';
+          } else {
+              msg = 'Uncaught Error.\n' + jqXHR.responseText;
+          }
+          $('#post').html(msg);
         }
       })
       
