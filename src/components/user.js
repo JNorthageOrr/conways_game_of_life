7dd4ae21d2562b1d32e796f02dce9f2db5893017
Justin NORR <jnorthageorr@gmail.com>
2018-04-08 15:04:24 -0400

    adds contentType to ajax user#create call

--- src/components/user.js
+++ src/components/user.js
@@ -79,6 +79,7 @@ class App extends Component {
       $.ajax({
         url: "http://45.55.72.9:3000/api/users",
         type: "POST",
+        contentType: "application/json; charset=utf-8",
         data: newRequest,
         dataType: "json",
         success: function (newResult) {
